7da7a58ad4980b483c111263aa596f5e6d904d22
sleeper <survivingknowledge@gmail.com>
2017-02-12 22:56:19 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
 
+  #shorthand for
+  # get '/admin/stats', to: 'stats#index'
+  # scope '/admin' do
+  #   resources :stats, only: [:index]
+  # end
+
+  #scope gives us stats_path
+  #moudle: 'admin' is telling this to use controllers in module/folder admin
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+
+  #this instead of stats_path, will generate admin_status_path
+  #namespace is shorthand for scope,module
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new, :edit]
   end
