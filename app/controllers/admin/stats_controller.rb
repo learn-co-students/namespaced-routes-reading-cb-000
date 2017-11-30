2a118ab8b5bfe9ddd0353eecebb46a7474988700
theboymo <billfero@gmail.com>
2017-11-30 20:15:25 +0000

    Done.

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,6 +1,7 @@
-class StatsController < ApplicationController
+class Admin::StatsController < ApplicationController
 
   def index
+    # you don't want your views to 'talk' to the db
     @post_count = Post.count
     @authors_count = Author.count
     @last_post = Post.last
